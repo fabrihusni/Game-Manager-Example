95d00bc3fb675e8d45e0d21a49fc379648a76f6f
fabrihusni <48792318@est.ort.edu.ar>
2024-08-15 12:03:46 -0300

    Clase 2

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+        DontDestroyOnLoad(this);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            ChangeScene("Game");
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ChangeScene("Menu");
+        }
+    }
+
+    public void ChangeScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
+
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+    public AudioSource Source;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+        DontDestroyOnLoad(this);
+
+        Source = GetComponent<AudioSource>();
+    }
+    public void PlaySound(AudioClip clip)
+    {
+        Source.Stop();
+        Source.loop = false;
+        Source.PlayOneShot(clip);
+    }
+
+}
